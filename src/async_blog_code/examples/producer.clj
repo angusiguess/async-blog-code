559246aa5af6a71a352e43b0e384a596156eb04b
Angus Fletcher <angusiguess@Anguss-MacBook-Pro.local>
2016-04-04 23:34:17 -0300

    Our first supervisor

--- src/async_blog_code/examples/producer.clj
+++ src/async_blog_code/examples/producer.clj
@@ -2,13 +2,42 @@
   (:require [async-blog-code.kafka-client :as client]
             [clojure.core.async :as a]))
 
-;; First one, no error handling.
+(defn publish-click
+  ;; The zero-arity version returns our initial state, sort of like a
+  ;; reducing function. This is so our go-loop doesn't need to know how
+  ;; to initialize state. We'll use this more in the future.
+  ([]
+   0)
+  ([connection offset publish>]
+   ;; Catch any errors thrown in this function.
+   (try (let [click (client/take! connection offset)]
+          (a/>!! publish> click)
+          ;; We tag any of our return values with a message type.
+          ;; For something as simple as this producer it isn't immediately useful
+          ;; But this will come in handy for more complex patterns.
+          [:read (inc offset)])
+        (catch Throwable e
+          [:error {:exception e
+                   :offset offset
+                   :operation :publish-click}]))))
 
-(defn start []
-  (let [connection (client/simple-consumer 10)
-        publish> (a/chan)]
-    (a/go-loop [offset 0]
-      (let [click (client/take! connection offset)]
-        (a/>! publish> click)
-        (recur (inc offset))))
+
+(defn start [publish>]
+  (let [connection (client/simple-consumer 10)]
+    (a/go-loop [offset (publish-click)]
+      ;; We use the message tag to determine if an error occurred. If it did, we return
+      ;; the message state.
+      (let [[msg-type state :as msg] (publish-click connection offset publish>)]
+        (if (= msg-type :error)
+          state
+          (recur state))))))
+
+(defn start-supervisor []
+  (let [publish> (a/chan 1024)]
+    (a/go-loop [producer (start publish>)]
+      (println "Starting producer.")
+      (let [error (a/<! producer)]
+        (println "An error has occurred: " error)
+        (println "Restarting producer.")
+        (recur (start publish>))))
     publish>))
